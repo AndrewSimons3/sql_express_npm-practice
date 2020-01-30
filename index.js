ba537a5e3cb52e2689216a3bae07a80cc7ad1e7e
AndrewSimons3 <simdrew07@gmail.com>
2020-01-30 14:12:57 -0600

    completed

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ let pool = mysql.createPool({
     connectionLimit: 100,
     host: '35.188.120.238',
     user: 'root',
-    database: 'Anj1',
+    database: 'Admin',
     debug: false
 });
 
@@ -22,8 +22,30 @@ function get_users(req, res) {
   })
 }
 
+
+
+function get_first_names(req, res) {
+  let sql = 'SELECT ??, ?? FROM ?? WHERE ?? < 10'
+  const replacements = ['id', 'first_name', 'users', 'id', 10]
+  sql = mysql.format(sql, replacements)
+  pool.query(sql, function(err, rows) {
+    if(err) {
+      return res.json({ 
+        'error': true,
+        'message': 'Error occured:' + err
+      })
+    } else {
+      res.json(rows)
+    }
+  })
+}
+
 app.get('/', function(req, res){
   get_users(req, res)
+});
+
+app.get('/first-names', function(req, res) {
+  get_first_names(req, res)
 })
 
 app.listen(port, () => {
